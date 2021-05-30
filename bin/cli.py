8bf77ae59316314317bd0eaf39cdd738ba30a111
Jason Russell <jasrusable@gmail.com>
2021-05-30 16:49:02 +0200

    Connect to socketio backend

--- bin/cli.py
+++ bin/cli.py
@@ -1,20 +1,37 @@
 import click
+import socketio
 
 
 @click.command()
 @click.option('-n', '--connector-name', default='Default connector', help='Name for the connector.')
 @click.option('-a', '--api-key', help='QueueMetrics.io organization API key. Get this from https://dashboard.queuemetrics.io')
-@click.option('-h', '--host', default="localhost", help='Redis host.')
-@click.option('-p', '--port', default="6379", help='Redis port.')
-@click.option('-d', '--database', default="0", help='Redis database.')
-@click.option('-w', '--password', help='Redis password.')
-@click.option('--tls', help='Use a TLS connection to Redis.')
-@click.option('-u', '--uri', help='Redis URI.')
-@click.option('-s', '--sentinels', help='Comma-seperated list of sentinel host/port pairs.')
-@click.option('-m', '--master', help='Name of master node used in sentinel configuration')
 @click.option('-b', '--backend', default="wss://api.queuemetrics.io", help='QueueMetrics backend.')
-def main(connector_name, api_key, host, port, database, password, tls, uri, sentinels, master, backend):
-    click.echo(f"Hello {connector_name}!")
+@click.option('-o', '--broker-url', help='Celery broker URL.')
+def main(connector_name, api_key, backend, broker_url):
+    websocketUri = backend
+
+    sio = socketio.Client()
+
+    @sio.event
+    def connect():
+        print("Socket connected to", websocketUri)
+        sio.emit('initialize-connector-connection', {
+            'apiKey': api_key,
+            'connectorType': 'celery',
+            'connectorName': connector_name,
+            'connectorVersion': '0.0.1'
+        })
+
+    @sio.event
+    def connect_error(data):
+        print("The connection failed!")
+
+    @sio.event
+    def disconnect():
+        print("Socket disconnected from", websocketUri)
+
+    print("Attempting to connect to", websocketUri)
+    sio.connect(websocketUri)
 
 
 if __name__ == '__main__':
